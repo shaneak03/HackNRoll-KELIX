fcc7c072233f4d0abd3ec68ac02ee3ede69470cb
Chern30 <148644407+Chern30@users.noreply.github.com>
2025-01-18 13:54:00 +0800

    Supabase Client

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import os
+from src.utils.supabase_client import supabase
+import pkg_resources
 
 API_KEY = os.getenv('API_KEY')
 print(API_KEY)
+print(supabase)
+
+installed_packages = pkg_resources.working_set
+installed_packages_list = sorted(["%s==%s" % (i.key, i.version) for i in installed_packages])
+print(installed_packages_list)
