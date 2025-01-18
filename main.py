7df62022d44dfacd5b35544c4c419bc6d67aaefe
shaneak <shaneak03@gmail.com>
2025-01-18 13:35:47 +0800

    setting up tele bot

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import os
 from dotenv import load_dotenv
+import telebot
 
+# Setting up .env and API_KEY
 load_dotenv()
 
 API_KEY = os.getenv('API_KEY')
 print(API_KEY)
+
+# Setting up the bot
+bot = telebot.TeleBot(API_KEY)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Welcome to the bot!")
+
+bot.polling()
