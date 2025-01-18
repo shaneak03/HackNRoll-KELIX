7844511397e235201b48e7eb7fde41f76b879cb2
shaneak <shaneak03@gmail.com>
2025-01-18 15:05:37 +0800

    settled imports

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import os
+from dotenv import load_dotenv
+import telebot
 
-API_KEY = os.getenv('API_KEY')
-print(API_KEY)
-print(supabase)
+load_dotenv()
 
-installed_packages = pkg_resources.working_set
-installed_packages_list = sorted(["%s==%s" % (i.key, i.version) for i in installed_packages])
-print(installed_packages_list)
+TELE_API_KEY = os.getenv('TELE_API_KEY')
+print(TELE_API_KEY)
 
 # Setting up the bot
-bot = telebot.TeleBot(API_KEY)
+bot = telebot.TeleBot(TELE_API_KEY)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
