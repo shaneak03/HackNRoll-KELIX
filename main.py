f1c60cbe040671ff69ea784e60ecea7c8c757308
shaneak <shaneak03@gmail.com>
2025-01-18 15:08:10 +0800

    imported supabase keys

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import telebot
 load_dotenv()
 
 TELE_API_KEY = os.getenv('TELE_API_KEY')
+SUPABASE_URL = os.getenv('SUPABASE_URL')
+SUPABASE_KEY = os.getenv('SUPABASE_KEY')
+
 print(TELE_API_KEY)
 
 # Setting up the bot
